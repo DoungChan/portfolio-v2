9b675242ed7b347bc2c5b4536fe15edb52fc3ade
DoungChan <sroeun.doungchan19@kit.edu.kh>
2024-04-25 20:28:26 +0700

    add exp

--- data/projectExp.js
+++ data/projectExp.js
@@ -1,64 +1,89 @@
 const projectExp = [
-  {
-    image: "/web3.png",
-    name: "Trading Bot Marketplace",
-    description:
-      "      Trading bot marketplace’s build as web 3.0 application objective is to be a first marketplace for trading bot that takes similarity to Opensea - a marketplace platform for NFT. Therefore we divided into 3 main sub objectives which are smart contract, frontend and backend developments. and using tecnnology such as Solidity, Reacjs, Nestjs, MogoDB, Firebase, Docker.  ",
+   {
+      image: "/web3.png",
+      name: "Trading Bot Marketplace",
+      description:
+         "      Trading bot marketplace’s build as web 3.0 application objective is to be a first marketplace for trading bot that takes similarity to Opensea - a marketplace platform for NFT. Therefore we divided into 3 main sub objectives which are smart contract, frontend and backend developments. and using tecnnology such as Solidity, Reacjs, Nestjs, MogoDB, Firebase, Docker.  ",
 
-    github: "https://github.com/DoungChan/Web3-Tradingbot-Frontend",
-    link: "https://tradingbothub.net/ ",
-  },
-  {
-    image: "/promokh.png",
-    name: "PromoKH",
-    description:
-      "PromoKh is a web application that allows users to create promotions for their products and services. The application allows users to create promotions for their products and services. The application allows users to create promotions for their products and services. The application allows users to create promotions for their products and services. Using Technology such as Nextjs, Spring Boot, MogoDB, Tailwindcss, Docker. ",
-    github: "https://github.com/DoungChan/B8-FullStack--Website--Group1",
-    link: " https://promokh.vercel.app/",
-  },
-  {
-    image: "/sms.png",
-    name: "School Manangement System",
-    description:
-      "School management system is a web application that helps school to manage their student, teacher, class, schedule, attendance, and report. This application is built with React, Typescript, Vite, Tailwindcss, Electron, Nodejs, Express, Postgresql, and Swagger.",
-    github: "https://github.com/incubation-center/Electron-DesktopApp---Group7",
-    link: " ",
-  },
+      github: "https://github.com/DoungChan/Web3-Tradingbot-Frontend",
+      link: "https://tradingbothub.net/ ",
+   },
+   {
+      image: "/promokh.png",
+      name: "PromoKH",
+      description:
+         "PromoKh is a web application that allows users to create promotions for their products and services. The application allows users to create promotions for their products and services. The application allows users to create promotions for their products and services. The application allows users to create promotions for their products and services. Using Technology such as Nextjs, Spring Boot, MogoDB, Tailwindcss, Docker. ",
+      github: "https://github.com/DoungChan/B8-FullStack--Website--Group1",
+      link: " https://promokh.vercel.app/",
+   },
+   {
+      image: "/sms.png",
+      name: "School Manangement System",
+      description:
+         "School management system is a web application that helps school to manage their student, teacher, class, schedule, attendance, and report. This application is built with React, Typescript, Vite, Tailwindcss, Electron, Nodejs, Express, Postgresql, and Swagger.",
+      github:
+         "https://github.com/incubation-center/Electron-DesktopApp---Group7",
+      link: " ",
+   },
 
-  {
-    image: "/efind.png",
-    name: "eFind",
-    description:
-      " eFind is a mobile application that allows users to find products, services, and shops around them. It is a platform where users can find products, services, and shops around them. It is a platform where users can find products, services, and shops around them. Technology used Flutter.",
-    github: "https://github.com/DoungChan/eFine_NewUI",
-    link: "",
-  },
-  {
-    image: "/vconnect.png",
-    name: "Vconnect",
-    description:
-      " The purpose of this app is to help people more easier in communication. It will allows user to create their QR Code that contain name, phone number, and other socail media name. Then when someone else scanning that QR Code it will auto saving those informations in their mobile phone. So they can easily contact each other. Technology used Flutter.",
-    github: "https://github.com/incubation-center/Flutter-app---Group3",
-    link: "",
-  },
-  {
-    image: "/order.jpg",
-    name: "Queue",
-    description:
-      " Queue is an adroid native application which is build help customer order queue in the shop, which mean the appiaction will alter whatever the order is reading, it will the make eviroement in the shop more quiet and peace, the application was build by using Kotlin, and FastApi.",
-    github:
-      "https://github.com/incubation-center/B8-Native--Mobile-Application--Group2",
-    link: "",
-  },
-  {
-    image: "/AI-Service.png",
-    name: "AI Recommendation Service",
-    description:
-      "I was challenge my self to build this site both UX/UI and frontend within 2 day. AI Recommendation Service is a web application that allows users to get recommendation of movies, books, and music. The application allows users to get recommendation of movies, books, and music. The application allows users to get recommendation of movies, books, and music. The application allows users to get recommendation of movies, books, and music.",
-    github: "https://github.com/DoungChan/AI-Services",
-    link: "https://ai-services-nine.vercel.app/ ",
-  },
-  // add more skills as needed
+   {
+      image: "/efind.png",
+      name: "eFind",
+      description:
+         " eFind is a mobile application that allows users to find products, services, and shops around them. It is a platform where users can find products, services, and shops around them. It is a platform where users can find products, services, and shops around them. Technology used Flutter.",
+      github: "https://github.com/DoungChan/eFine_NewUI",
+      link: "",
+   },
+   {
+      image: "/vconnect.png",
+      name: "Vconnect",
+      description:
+         " The purpose of this app is to help people more easier in communication. It will allows user to create their QR Code that contain name, phone number, and other socail media name. Then when someone else scanning that QR Code it will auto saving those informations in their mobile phone. So they can easily contact each other. Technology used Flutter.",
+      github: "https://github.com/incubation-center/Flutter-app---Group3",
+      link: "",
+   },
+   {
+      image: "/order.jpg",
+      name: "Queue",
+      description:
+         " Queue is an adroid native application which is build help customer order queue in the shop, which mean the appiaction will alter whatever the order is reading, it will the make eviroement in the shop more quiet and peace, the application was build by using Kotlin, and FastApi.",
+      github:
+         "https://github.com/incubation-center/B8-Native--Mobile-Application--Group2",
+      link: "",
+   },
+   {
+      image: "/AI-Service.png",
+      name: "AI Recommendation Service",
+      description:
+         "I was challenge my self to build this site both UX/UI and frontend within 2 day. AI Recommendation Service is a web application that allows users to get recommendation of movies, books, and music. The application allows users to get recommendation of movies, books, and music. The application allows users to get recommendation of movies, books, and music. The application allows users to get recommendation of movies, books, and music.",
+      github: "https://github.com/DoungChan/AI-Services",
+      link: "https://ai-services-nine.vercel.app/ ",
+   },
+   {
+      image: "/Home-M-POS.png",
+      name: "M-POS Landing Page",
+      description:
+         "Fully responsive landing page for M-POS which is designed to support and promote a mobile point-of-sale (M-POS) system. The landing page showcases the features and benefits of the M-POS system, highlighting its ease of use, security, and convenience for businesses of all sizes. With a modern and sleek design, the landing page effectively communicates the value proposition of the M-POS system to potential customers. Built with Vue2, the landing page ensures a smooth and interactive user experience across different devices and screen sizes. biuld with Vuejs.",
+      github: "",
+      link: "http://13.228.102.178:6002/ ",
+   },
+   {
+      image: "/SKYBOOKING.png",
+      name: "SkyBooking",
+      description:
+         "I built a flight and hotel booking application as an H5 (HTML5) application. This application allows users to search and book flights and hotels for their travel needs. Users can specify their travel dates, destinations, and preferences to find the best flight and hotel options. The application provides a user-friendly interface with intuitive search filters and sorting options. It also integrates with external APIs to fetch real-time flight and hotel data. The application is built using HTML5, CSS3, and JavaScript, ensuring cross-platform compatibility and responsive design. ",
+      github: "",
+      link: "https://test-h5.skybooking.cc/",
+   },
+   {
+      image: "/skybooking-backoffice.png",
+      name: "SkyBooking",
+      description:
+         "The Back Office Dashboard is a web application that provides administrators with a centralized platform to manage and monitor various aspects of the system. It includes features such as user management, content management, analytics, and reporting. The dashboard is built using React, Node.js, Express, MongoDB, and Material-UI.",
+      github: "",
+      link: "https://test-bo.skybooking.cc/auth/login",
+   },
+   // add more skills as needed
 ];
 
 export default projectExp;
